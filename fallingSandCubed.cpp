ce1b659d22e18362792b48455c72391937efcf1f
unknown <devon.l.mckee@gmail.com>
2021-11-18 18:24:43 -0800

    Initial commit with basic setup

---
+++ fallingSandCubed.cpp
@@ -0,0 +1,122 @@
+// fallingSandCubed - Devon McKee, Michael Pablo
+
+#define _USE_MATH_DEFINES
+
+#include <glad.h>
+#include <GLFW/glfw3.h>
+#include <time.h>
+#include <vector>
+#include "VecMat.h"
+#include "Camera.h"
+#include "Misc.h"
+#include "GLXtras.h"
+#include "GeomUtils.h"
+
+using std::vector;
+
+GLuint vBuffer = 0;
+GLuint program = 0;
+
+int win_width = 800, win_height = 800;
+
+Camera camera((float)win_width / win_height, vec3(0, 0, 0), vec3(0, 0, -5));
+vec3 lightSource = vec3(1, 1, 0);
+
+enum ParticleType {
+	SAND, WATER, OIL, SALT
+};
+
+struct Particle {
+
+};
+
+struct ParticleGrid {
+	// include function to write grid to GPU memory
+};
+
+const char* vertShader = R"()";
+
+const char* fragShader = R"()";
+
+void Resize(GLFWwindow* window, int width, int height) {
+	camera.Resize(win_width = width, win_height = height);
+	glViewport(0, 0, win_width, win_height);
+}
+
+void Keyboard(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	if (key == GLFW_KEY_ESCAPE || key == GLFW_KEY_Q) {
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+}
+
+bool Shift(GLFWwindow* w) {
+	return glfwGetKey(w, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS ||
+		glfwGetKey(w, GLFW_KEY_RIGHT_SHIFT) == GLFW_PRESS;
+}
+
+void MouseButton(GLFWwindow* w, int butn, int action, int mods) {
+	double x, y;
+	glfwGetCursorPos(w, &x, &y);
+	y = win_height - y;
+	if (action == GLFW_PRESS)
+		camera.MouseDown((int)x, (int)y);
+	if (action == GLFW_RELEASE)
+		camera.MouseUp();
+}
+
+void MouseMove(GLFWwindow* w, double x, double y) {
+	if (glfwGetMouseButton(w, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) { // drag
+		y = win_height - y;
+		camera.MouseDrag((int)x, (int)y, Shift(w));
+	}
+}
+
+void MouseWheel(GLFWwindow* w, double ignore, double spin) {
+	camera.MouseWheel(spin > 0, Shift(w));
+}
+
+void InitVertexBuffer() {
+	glGenBuffers(1, &vBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vBuffer);
+}
+
+void Display() {
+	glClear(GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glFlush();
+}
+
+int main() {
+	srand((int)time(NULL));
+	if (!glfwInit())
+		return 1;
+	GLFWwindow* window = glfwCreateWindow(win_width, win_height, "Falling Sand: Cubed", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		return 1;
+	}
+	glfwSetWindowPos(window, 100, 100);
+	glfwMakeContextCurrent(window);
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+	PrintGLErrors();
+	if (!(program = LinkProgramViaCode(&vertShader, &fragShader)))
+		return 1;
+	InitVertexBuffer();
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwSetCursorPosCallback(window, MouseMove);
+	glfwSetMouseButtonCallback(window, MouseButton);
+	glfwSetScrollCallback(window, MouseWheel);
+	glfwSetKeyCallback(window, Keyboard);
+	glfwSetWindowSizeCallback(window, Resize);
+	glfwSwapInterval(1);
+	while (!glfwWindowShouldClose(window)) {
+		Display();
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glDeleteBuffers(1, &vBuffer);
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
