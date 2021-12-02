e411c8ae3b533fcfdab729c1c40454cca0e9da8e
unknown <devon.l.mckee@gmail.com>
2021-12-01 18:46:24 -0800

    Adding some randomness to liquid movement, needs to be refined

--- computeShader.glsl
+++ computeShader.glsl
@@ -18,9 +18,23 @@ const int P_SAND = 3;
 const int P_OIL = 4;
 const int P_SALT = 5;
 
+ivec3 inv = ivec3(gl_GlobalInvocationID);
+
 // Pseudorandom values for more natural processing
-uint rand(vec2 co, uint rmin = 0, uint rmax = 1){
-    return uint(round( fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453) * rmax )) + rmin;
+uint pcg_hash(uint input) {
+    uint state = input * 747796405u + 2891336453u;
+    uint word = ((state >> ((state >> 28u) + 4u)) ^ state) * 277803737u;
+    return (word >> 22u) ^ word;
+}
+
+void shuffleMoves(inout uint[8] arr) {
+	for (uint i = 0; i < 8; i++) {
+		uint a = uint(floor((float(pcg_hash(inv.x + i)) / 4294967296.0) * 8.0));
+		uint b = uint(floor((float(pcg_hash(inv.y - i)) / 4294967296.0) * 8.0));
+		uint c = arr[a];
+		arr[a] = arr[b];
+		arr[b] = c;
+	}
 }
 
 bool isLiquid(uint p) {
@@ -69,6 +83,49 @@ void processLiquids(uint x, uint y, uint z) {
 		}
 	}
 	// Else, try to move sideways
+	uint[8] moves;
+	for (uint i = 0; i < 8; i++) moves[i] = i;
+	shuffleMoves(moves);
+	for (uint m = 0; m < 8; m++) {
+		ivec3 move;
+		switch(moves[m]) {
+			case 0:
+				move = ivec3(x - 1, y, z - 1);
+				break;
+			case 1:
+				move = ivec3(x, y, z - 1);
+				break;
+			case 2:
+				move = ivec3(x + 1, y, z - 1);
+				break;
+			case 3:
+				move = ivec3(x - 1, y, z);
+				break;
+			case 4:
+				move = ivec3(x + 1, y, z);
+				break;
+			case 5:
+				move = ivec3(x - 1, y, z + 1);
+				break;
+			case 6:
+				move = ivec3(x, y, z + 1);
+				break;
+			case 7:
+				move = ivec3(x + 1, y, z + 1);
+				break;
+		}
+		if (move.x >= 0 && move.x < GRID_SIZE && move.z >= 0 && move.z < GRID_SIZE) {
+			uint np = srcGrid[move.x][move.y][move.z];
+			if (np == P_AIR || isGas(np)) {
+				uint wp = atomicCompSwap(dstGrid[move.x][move.y][move.z], np, p);
+				if (wp == np) {
+					atomicExchange(dstGrid[x][y][z], np);
+					return;
+				}
+			}
+		}
+	}
+	/*
 	for (int nx = 1; nx >= -1; nx--) {
 		for (int nz = 1; nz >= -1; nz--) {
 			if ((nx != 0 || nz != 0) && x + nx < GRID_SIZE && x + nx >= 0 && z + nz < GRID_SIZE && z + nz >= 0) {
@@ -83,6 +140,7 @@ void processLiquids(uint x, uint y, uint z) {
 			}
 		}
 	}
+	*/
 }
 
 void processMovableSolids(uint x, uint y, uint z) {
@@ -124,7 +182,6 @@ void processGases(uint x, uint y, uint z) {
 }
 
 void main() {
-	ivec3 inv = ivec3(gl_GlobalInvocationID);
 	uint p = srcGrid[inv.x][inv.y][inv.z];
 	if (isLiquid(p)) {
 		processLiquids(inv.x, inv.y, inv.z);
